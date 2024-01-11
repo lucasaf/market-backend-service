5de8b697437957d14e9ed53ee71c0e010df63bd9
Lucas <ferreira.lucasalves@gmail.com>
2024-01-11 15:57:03 -0300

    update unit tests

--- src/products/services/product.service.spec.ts
+++ src/products/services/product.service.spec.ts
@@ -1,15 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CreateProductDto } from '../dto/create-product.dto';
 import { Product } from '../entities/product.entity';
-import { CREATE_PRODUCT_USE_CASE } from '../shared/constants';
+import {
+  CREATE_PRODUCT_USE_CASE,
+  UPDATE_PRODUCT_USE_CASE,
+} from '../shared/constants';
 import { ProductService } from './product.service';
 
 let productService: ProductService;
 let mockCreateProductUseCase;
+let mockUpdateProductUseCase;
 
 beforeEach(async () => {
   mockCreateProductUseCase = {
     execute: jest.fn().mockResolvedValue(new Product()),
+  };
+  mockUpdateProductUseCase = {
+    execute: jest.fn().mockResolvedValue({ id: '123', ...new Product() }),
   };
 
   const module: TestingModule = await Test.createTestingModule({
@@ -19,21 +26,46 @@ beforeEach(async () => {
         provide: CREATE_PRODUCT_USE_CASE,
         useValue: mockCreateProductUseCase,
       },
+      {
+        provide: UPDATE_PRODUCT_USE_CASE,
+        useValue: mockUpdateProductUseCase,
+      },
     ],
   }).compile();
 
   productService = module.get<ProductService>(ProductService);
 });
 
-it('should create a product', async () => {
-  const createProductDto: CreateProductDto = {
-    name: 'Test Product',
-    price: 10,
-    type: 'type',
-  };
+describe('create', () => {
+  it('should create a product', async () => {
+    const createProductDto: CreateProductDto = {
+      name: 'Test Product',
+      price: 10,
+      type: 'type',
+    };
 
-  await productService.createProduct(createProductDto);
-  expect(mockCreateProductUseCase.execute).toHaveBeenCalledWith(
-    createProductDto,
-  );
+    await productService.createProduct(createProductDto);
+
+    expect(mockCreateProductUseCase.execute).toHaveBeenCalledWith(
+      createProductDto,
+    );
+  });
+
+  describe('update', () => {
+    it('should update a product', async () => {
+      const id = '123';
+      const updateProductDto = {
+        name: 'Test Product',
+        price: 10,
+        type: 'type',
+      };
+
+      await productService.updateProduct(id, updateProductDto);
+
+      expect(mockUpdateProductUseCase.execute).toHaveBeenCalledWith(
+        id,
+        updateProductDto,
+      );
+    });
+  });
 });
