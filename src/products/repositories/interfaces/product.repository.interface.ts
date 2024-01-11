5babb186deea08c5582c38a9975e29f2a8a621f0
Lucas <ferreira.lucasalves@gmail.com>
2024-01-11 15:56:38 -0300

    add update product

--- src/products/repositories/interfaces/product.repository.interface.ts
+++ src/products/repositories/interfaces/product.repository.interface.ts
@@ -1,6 +1,8 @@
 import { CreateProductDto } from '../../dto/create-product.dto';
+import { UpdateProductDto } from '../../dto/update-product.dto';
 import { Product } from '../../entities/product.entity';
 
 export interface IProductRepository {
   createProduct(createProductDto: CreateProductDto): Promise<Product>;
+  updateProduct(id: string, updateProductDto: UpdateProductDto);
 }
