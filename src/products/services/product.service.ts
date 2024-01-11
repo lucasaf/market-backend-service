ccfca5c3337cdbf733aa03bf15d54a86aba79e60
Lucas <ferreira.lucasalves@gmail.com>
2024-01-11 15:57:15 -0300

    add updateProduct method

--- src/products/services/product.service.ts
+++ src/products/services/product.service.ts
@@ -1,16 +1,27 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { CreateProductDto } from '../dto/create-product.dto';
-import { CREATE_PRODUCT_USE_CASE } from '../shared/constants';
+import { UpdateProductDto } from '../dto/update-product.dto';
+import {
+  CREATE_PRODUCT_USE_CASE,
+  UPDATE_PRODUCT_USE_CASE,
+} from '../shared/constants';
 import { ICreateProductUseCase } from '../use-cases/interfaces/create-product.use-case.interface';
+import { IUpdateProductUseCase } from '../use-cases/interfaces/update-product.use-case.interface';
 
 @Injectable()
 export class ProductService {
   constructor(
     @Inject(CREATE_PRODUCT_USE_CASE)
     private readonly createProductUseCase: ICreateProductUseCase,
+    @Inject(UPDATE_PRODUCT_USE_CASE)
+    private readonly updateProductUseCase: IUpdateProductUseCase,
   ) {}
 
-  createProduct(createProductDto: CreateProductDto) {
-    return this.createProductUseCase.execute(createProductDto);
+  async createProduct(createProductDto: CreateProductDto) {
+    return await this.createProductUseCase.execute(createProductDto);
+  }
+
+  async updateProduct(id: string, updateProductDto: UpdateProductDto) {
+    return await this.updateProductUseCase.execute(id, updateProductDto);
   }
 }
